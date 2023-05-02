2a6785aca5db726a6421bff4781eda37be179c48
Adithya S K <adithyaskolavi@gmail.com>
2023-05-02 12:18:21 +0530

    shifter from npm to yarn with tailwind css - Adithya S K

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      primary_green: '#006241',
-      secondary_green: '#1BA979',
-      primary_blue: '#332FE8',
-      gray: '#475E6B',
-      background: '#F5F6FA',
-      primary_font: '#191D20',
-      secondary_font: '#333333',
-      tertiary_font: '#6E757E',
-      button_black: '#282730',
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
-    fontFamily: {
-      sans: ['Inter', 'sans-serif'],
-    },
-    extend: {},
   },
   plugins: [],
-};
+}
