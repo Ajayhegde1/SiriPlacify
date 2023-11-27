d10ec7dcff35189761449d85a7565a77efd1df7f
advert-ease <pvladvertease@gmail.com>
2023-11-27 17:37:30 +0530

    signinpage register link

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,61 +1,55 @@
-import {useRouter} from 'next/router'
-import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import { openNotification, notificationTypes } from '@/utils/notifications'
+import { openNotification, notificationTypes } from "@/utils/notifications";
 
-import { routes } from '@/constants/routes'
-import DocHeader from '@/components/DocHeader'
-import Navbar from '@/components/NavBar'
-import OpeningSection from '@/components/OpeningSection'
-import SecondSection from '@/components/SecondSection'
-import ThirdSection from '@/components/ThirdSection'
-import ContactSection from '@/components/ContactSection'
-import FooterSection from '@/components/FooterSection'
+import { routes } from "@/constants/routes";
+import DocHeader from "@/components/DocHeader";
+import Navbar from "@/components/NavBar";
+import OpeningSection from "@/components/OpeningSection";
+import SecondSection from "@/components/SecondSection";
+import ThirdSection from "@/components/ThirdSection";
+import ContactSection from "@/components/ContactSection";
+import FooterSection from "@/components/FooterSection";
+import Sidebar from "@/components/SideBar";
 
-export default function Home () {
-  const user = useSelector(state => state.user)
-  const router = useRouter()
+export default function Home() {
+  const user = useSelector((state) => state.user);
+  const router = useRouter();
 
   useEffect(() => {
-    if (user === null){
-      router.push(routes.HOMEPAGE)
+    if (user === null) {
+      router.push(routes.HOMEPAGE);
+    } else if (user.accType === "0") {
+      router.push(routes.TPODASHBOARD);
+    } else if (user.accType === "1") {
+      router.push(routes.JOBS);
+    } else if (user.accType === "2") {
+      router.push(routes.COMPANYDASHBOARD);
+    } else {
+      openNotification(notificationTypes.ERROR, "Error", "Unauthorized User");
     }
-    else if (user.accType === '0'){
-      router.push(routes.TPODASHBOARD)
-    }
-    else if (user.accType === '1') {
-      router.push(routes.JOBS)
-    }
-    else if (user.accType === '2') {
-      router.push(routes.COMPANYDASHBOARD)
-    }
-    else{
-      openNotification(
-        notificationTypes.ERROR,
-        'Error',
-        'Unauthorized User'
-      )
-    }
-  }, [user])
+  }, [user]);
 
   return (
     <div>
-      <DocHeader DocTitle='Placify.io' />
+      <DocHeader DocTitle="Placify.io" />
       <Navbar />
-      <div id='Home'>
+      <div id="Home">
         <OpeningSection />
       </div>
-      <div id='About'>
+
+      <div id="About">
         <SecondSection />
       </div>
-      <div id='Portfolio'>
+      <div id="Portfolio">
         <ThirdSection />
       </div>
-      <div id='contact'>
+      <div id="contact">
         <ContactSection />
       </div>
       <FooterSection />
     </div>
-  )
+  );
 }
--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -1,130 +1,128 @@
-import student from '../public/students.png'
+import student from "../public/students.png";
 
-import Image from 'next/image'
-import { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import DocHeader from '@/components/DocHeader'
-import TextField from '@/components/InputComponents/TextField'
-import Button from '@/components/Buttons'
-import ForgotPasswordModal from '@/components/Modal/ForgotPasswordModal'
+import DocHeader from "@/components/DocHeader";
+import TextField from "@/components/InputComponents/TextField";
+import Button from "@/components/Buttons";
+import ForgotPasswordModal from "@/components/Modal/ForgotPasswordModal";
 
-import { signIn } from '@/redux/Slices/userSlice'
-import { useRouter } from 'next/router'
-import { Spin } from 'antd'
-import { notificationTypes, openNotification } from '@/utils/notifications'
-import { routes } from '@/constants/routes'
+import { signIn } from "@/redux/Slices/userSlice";
+import { useRouter } from "next/router";
+import { Spin } from "antd";
+import { notificationTypes, openNotification } from "@/utils/notifications";
+import { routes } from "@/constants/routes";
+import Link from "next/link";
 
-export default function CollegeProfile () {
-  const dispatch = useDispatch()
-  const router = useRouter()
+export default function CollegeProfile() {
+  const dispatch = useDispatch();
+  const router = useRouter();
 
-  const user = useSelector((state) => state.user)
+  const user = useSelector((state) => state.user);
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [isBtnDisabled, setIsBtnDisabled] = useState(true)
-  const [btnText, setBtnText] = useState('Sign In')
-  const [isLoading, setLoading] = useState(false)
-  const [showModal, setShowModal] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+  const [btnText, setBtnText] = useState("Sign In");
+  const [isLoading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     if (email.length > 0 && password.length > 0) {
-      setIsBtnDisabled(false)
+      setIsBtnDisabled(false);
     } else {
-      setIsBtnDisabled(true)
+      setIsBtnDisabled(true);
     }
-  }, [email, password])
+  }, [email, password]);
 
   useEffect(() => {
     if (user) {
-      if (user.accType === '0'){
-        router.push(routes.TPODASHBOARD)
-      }
-      else if (user.accType === '1') {
-        router.push(routes.JOBS)
-      }
-      else if (user.accType === '2') {
-        router.push(routes.COMPANYDASHBOARD)
-      }
-      else{
-        openNotification(
-          notificationTypes.ERROR,
-          'Error',
-          'Unauthorized User'
-        )
+      if (user.accType === "0") {
+        router.push(routes.TPODASHBOARD);
+      } else if (user.accType === "1") {
+        router.push(routes.JOBS);
+      } else if (user.accType === "2") {
+        router.push(routes.COMPANYDASHBOARD);
+      } else {
+        openNotification(notificationTypes.ERROR, "Error", "Unauthorized User");
       }
     }
-  }, [user])
+  }, [user]);
 
   const handleLogin = (e) => {
-    e.preventDefault()
-    setLoading(true)
-    dispatch(signIn({ email, password }))
-    setLoading(false)
-
-  }
+    e.preventDefault();
+    setLoading(true);
+    dispatch(signIn({ email, password }));
+    setLoading(false);
+  };
 
   return (
     <div>
-      <DocHeader
-        DocTitle='Sign In'
-      />
-      <div className='ml-5 lg:ml-0 grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-4 xl:gap-6 2xl:gap-12'>
-        <div className='hidden lg:block min-h-screen'>
-          <Image className='h-full w-full' src={student} alt='students' />
+      <DocHeader DocTitle="Sign In" />
+      <div className="ml-5 lg:ml-0 grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-4 xl:gap-6 2xl:gap-12">
+        <div className="hidden lg:block min-h-screen">
+          <Image className="h-full w-full" src={student} alt="students" />
         </div>
-        <div className='mt-4 py-4 md:py-12 ml-0 md:ml-4 xl:ml-6 mr-8 md:mr-10 xl:mr-12 2xl:mr-16'>
-          <h1 className='text-center md:text-left mb-10 mt-12 text-3xl md:text-4xl font-Heading font-semibold text-green-800'>Login</h1>
+        <div className="mt-4 py-4 md:py-12 ml-0 md:ml-4 xl:ml-6 mr-8 md:mr-10 xl:mr-12 2xl:mr-16">
+          <h1 className="text-center md:text-left mb-10 mt-12 text-3xl md:text-4xl font-Heading font-semibold text-green-800">
+            Login
+          </h1>
           <form onSubmit={handleLogin}>
             <TextField
-              label='Email ID'
-              placeholder='sample@gmail.com'
-              type='text'
+              label="Email ID"
+              placeholder="sample@gmail.com"
+              type="text"
               value={email}
               onChangeHandler={(e) => setEmail(e.target.value)}
               required
             />
             <TextField
-              label='Password'
-              placeholder='********'
-              type='password'
+              label="Password"
+              placeholder="********"
+              type="password"
               value={password}
               onChangeHandler={(e) => setPassword(e.target.value)}
               required
             />
-            <div className='flex gap-2'>              
-                {
-                    isLoading ? <Spin size='large' />
-              :<Button
-              type='submit'
-              onClickHandler={handleLogin}
-              disabled={isBtnDisabled}
-              btnText={btnText}
-            />  }
+            <div className="flex gap-2">
+              {isLoading ? (
+                <Spin size="large" />
+              ) : (
+                <Button
+                  type="submit"
+                  onClickHandler={handleLogin}
+                  disabled={isBtnDisabled}
+                  btnText={btnText}
+                />
+              )}
             </div>
           </form>
-          <div className='cursor-pointer mt-6 text-left'>
+          <div className="cursor-pointer mt-6 text-left">
             <div onClick={() => setShowModal(!showModal)}>
-              <p className='text-base font-medium'>
-                <span className='text-red-700 hover:text-red-500 hover:border-b-2 hover:border-red-500'>Forgot Password?</span>
+              <p className="text-base font-medium">
+                <span className="text-red-700 hover:text-red-500 hover:border-b-2 hover:border-red-500">
+                  Forgot Password?
+                </span>
               </p>
             </div>
           </div>
-          <div className='border-2 border-gray-300 shadow-lg rounded-lg mt-6 p-8'>
-            <p className='font-bold text-center md:text-left text-sm md:text-base font-Body text-black'>
+          <div className="border-2 border-gray-300 shadow-lg rounded-lg mt-6 p-8">
+            <p className="font-bold text-center md:text-left text-sm md:text-base font-Body text-black">
               New to Placify?
             </p>
-            <div className='mt-5 text-center bg-customLightBlue p-5 rounded-lg'>
-              <p className='text-blue-400 text-blue font-bold font-heading'>Register for demo </p>
-            </div>
+            <Link href="">
+              <div className="mt-5 text-center bg-customLightBlue p-5 rounded-lg hover:bg-blue-100">
+                <p className="text-blue-400 text-blue font-bold font-heading">
+                  Register for demo{" "}
+                </p>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
-      <ForgotPasswordModal
-        showModal={showModal}
-        setShowModal={setShowModal}
-      />
+      <ForgotPasswordModal showModal={showModal} setShowModal={setShowModal} />
     </div>
-  )
+  );
 }
