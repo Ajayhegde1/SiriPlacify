56bde143b09513ba1493f23f610e0f974f18b4e0
advert-ease <pvladvertease@gmail.com>
2024-02-20 11:47:14 +0530

    hardcoded the avergae highest emdian

--- components/Dashboard/SalaryStatsComponents/index.jsx
+++ components/Dashboard/SalaryStatsComponents/index.jsx
@@ -6,25 +6,74 @@ import { useState, useEffect } from "react";
 export default function SalaryStatsComponents() {
   const [salaryStats, setSalaryStats] = useState([]);
 
-  useEffect(() => {
-    getSalaryStats()
-      .then((res) => {
-        if (res.data.status === 200) {
-          setSalaryStats(res.data.data);
-        } else {
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
+  // useEffect(() => {
+  //   getSalaryStats()
+  //     .then((res) => {
+  //       if (res.data.status === 200) {
+  //         setSalaryStats(res.data.data);
+  //       } else {
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+  const hardcodevalues = [
+    { id: 1, value: 12, title: "Average" },
+    { id: 2, value: 8, tite: "Median" },
+    { id: 3, value: 25, title: "Highest" },
+  ];
   return (
     <div className="mt-4 mb-6 p-4 bg-customOrange rounded-lg">
       <h1 className="text-left pt-2 text-lg font-semibold text-black mb-4">
         Salary package offered
       </h1>
-      {salaryStats !== null || Object?.keys(salaryStats).length === 0 ? (
+
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
+        <div className={`flex gap-4 border-r-$ md:border-r-2 border-black`}>
+          <h1
+            className={`text-xl md:text-3xl text-black font-bold $
+         `}
+          >
+            12
+          </h1>
+          <div>
+            <p>Average</p>
+            <p className="text-sm text-black font-semibold">
+              (Per annum – in lakh)
+            </p>
+          </div>
+        </div>
+        <div className={`flex gap-4 border-r-$ md:border-r-2 border-black`}>
+          <h1
+            className={`text-xl md:text-3xl text-black font-bold $
+         `}
+          >
+            8
+          </h1>
+          <div>
+            <p>Highest</p>
+            <p className="text-sm text-black font-semibold">
+              (Per annum – in lakh)
+            </p>
+          </div>
+        </div>
+        <div className={`flex gap-4 border-r-$ md:border-r-2 border-black`}>
+          <h1
+            className={`text-xl md:text-3xl text-black font-bold $
+         `}
+          >
+            25
+          </h1>
+          <div>
+            <p>Average</p>
+            <p className="text-sm text-black font-semibold">
+              (Per annum – in lakh)
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* {salaryStats !== null || Object?.keys(salaryStats).length === 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
           {Object.entries(salaryStats).map(([key, value]) => (
             <div
@@ -73,21 +122,18 @@ export default function SalaryStatsComponents() {
               </h1>
               <div className={`pr-${key === "medianPackage" ? "2" : "4"}`}>
                 <p className="text-md text-black font-semibold">
-                {key === "meanPackage"
+                  {key === "meanPackage"
                     ? "Average"
                     : key === "medianPackage"
                     ? "Median"
-                    : "Highest"
-                }
+                    : "Highest"}
                 </p>
-                <p className="text-sm text-black font-semibold">
-                  (Per annum)
-                </p>
+                <p className="text-sm text-black font-semibold">(Per annum)</p>
               </div>
             </div>
           ))}
         </div>
-      )}
+      )} */}
     </div>
   );
 }
